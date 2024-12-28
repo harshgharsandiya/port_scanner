ac639e85ba67a8bbde93a4f1fed20f92e1d700d5
harshgharsandiya <harshgharsandiya@gmail.com>
2024-12-28 15:21:19 +0530

    clean up

--- banner.py
+++
@@ -1,9 +0,0 @@
-import pyfiglet
-from datetime import datetime
-
-def printBanner(text, target):
-    print(pyfiglet.figlet_format(text))
-    print("-"*50)
-    print("Scanning Target: " + target)
-    print("Scanning started at: " + str(datetime.now()))
-    print("-"*50)
